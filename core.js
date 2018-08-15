50a13ea2b6582394e696341a3597664877f6ae0e
CI <irsooti+ci@gmail.com>
2018-08-15 21:51:06 +0000

    Updates

--- core.js
+++ core.js
@@ -1,70 +1,78 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 var core = function core() {
-  var isDebugging = false;
+    var isDebugging = false;
+    var parseError = "Item found can't be converted. Maybe it was stored using legacy localStorage. \n    Anyway, as fallback, I returned the result of localStorage.getItem";
 
-  var setDebuggingStatus = function setDebuggingStatus(bool) {
-    return isDebugging = bool === true;
-  };
-  var showDebuggingTable = function showDebuggingTable() {
-    if (isDebugging) {
-      console.table(localStorage);
-    }
-  };
+    var setDebuggingStatus = function setDebuggingStatus(bool) {
+        return isDebugging = bool === true;
+    };
+    var showDebuggingTable = function showDebuggingTable() {
+        if (isDebugging) {
+            console.table(localStorage);
+        }
+    };
 
-  var tryGetValue = function tryGetValue(item) {
-    var itemFound = localStorage.getItem(item);
-    if (itemFound === null) return itemFound;
+    var tryGetValue = function tryGetValue(item) {
+        var itemFound = localStorage.getItem(item);
+        if (itemFound === null) return itemFound;
 
-    showDebuggingTable();
-    return JSON.parse(itemFound);
-  };
+        var tryParseResult = null;
 
-  var setValue = function setValue(key, item) {
-    localStorage.setItem(key, JSON.stringify(item));
-    showDebuggingTable();
-  };
+        try {
+            tryParseResult = JSON.parse(itemFound);
+        } catch (e) {
+            console.warn(parseError);
+            tryParseResult = itemFound;
+        }
+        return tryParseResult;
+    };
 
-  var removeValue = function removeValue(key) {
-    localStorage.removeItem(key);
-    showDebuggingTable();
-  };
+    var setValue = function setValue(key, item) {
+        localStorage.setItem(key, JSON.stringify(item));
+        showDebuggingTable();
+    };
 
-  var reset = function reset() {
-    localStorage.clear();
-    showDebuggingTable();
-  };
+    var removeValue = function removeValue(key) {
+        localStorage.removeItem(key);
+        showDebuggingTable();
+    };
 
-  return {
-    /**
-     *    console.table() your current localStorage
-     *    Set true or false to enable/disable
-     */
-    setDebuggingStatus: setDebuggingStatus,
-    /**
-     *    It try to access to corresponding property like
-     *    localStorage.getItem
-     */
-    tryGetValue: tryGetValue,
+    var reset = function reset() {
+        localStorage.clear();
+        showDebuggingTable();
+    };
 
-    /**
-     *    It sets a new json compliant object
-     */
-    setValue: setValue,
+    return {
+        /**
+         *    console.table() your current localStorage
+         *    Set true or false to enable/disable
+         */
+        setDebuggingStatus: setDebuggingStatus,
+        /**
+         *    It try to access to corresponding property like
+         *    localStorage.getItem
+         */
+        tryGetValue: tryGetValue,
 
-    /**
-     *    It removes the matching property value
-     */
-    removeValue: removeValue,
+        /**
+         *    It sets a new json compliant object
+         */
+        setValue: setValue,
 
-    /**
-     *    It resets localStorage
-     */
-    reset: reset
-  };
+        /**
+         *    It removes the matching property value
+         */
+        removeValue: removeValue,
+
+        /**
+         *    It resets localStorage
+         */
+        reset: reset
+    };
 };
 
 exports.default = core;
