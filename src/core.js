49fcd23c4aa511bce3d6fc1a4ab79914b66aae75
D I <irsooti@gmail.com>
2018-08-15 23:50:39 +0200

    some fix

--- src/core.js
+++ src/core.js
@@ -1,63 +1,72 @@
 const core = () => {
-  let isDebugging = false;
+    let isDebugging = false;
+    const parseError = `Item found can\'t be converted. Maybe it was stored using legacy localStorage. 
+    Anyway, as fallback, I returned the result of localStorage.getItem`;
 
-  let setDebuggingStatus = bool => (isDebugging = bool === true);
-  let showDebuggingTable = () => {
-    if (isDebugging) {
-      console.table(localStorage);
-    }
-  };
+    let setDebuggingStatus = bool => (isDebugging = bool === true);
+    let showDebuggingTable = () => {
+        if (isDebugging) {
+            console.table(localStorage);
+        }
+    };
 
-  let tryGetValue = item => {
-    var itemFound = localStorage.getItem(item);
-    if (itemFound === null) return itemFound;
+    let tryGetValue = item => {
+        var itemFound = localStorage.getItem(item);
+        if (itemFound === null) return itemFound;
 
-    showDebuggingTable();
-    return JSON.parse(itemFound);
-  };
+        let tryParseResult = null;
 
-  let setValue = function(key, item) {
-    localStorage.setItem(key, JSON.stringify(item));
-    showDebuggingTable();
-  };
+        try {
+            tryParseResult = JSON.parse(itemFound);
+        } catch (e) {
+            console.warn(parseError);
+            tryParseResult = itemFound;
+        }
+        return tryParseResult;
+    };
 
-  let removeValue = key => {
-    localStorage.removeItem(key);
-    showDebuggingTable();
-  };
+    let setValue = function(key, item) {
+        localStorage.setItem(key, JSON.stringify(item));
+        showDebuggingTable();
+    };
 
-  let reset = () => {
-    localStorage.clear();
-    showDebuggingTable();
-  };
+    let removeValue = key => {
+        localStorage.removeItem(key);
+        showDebuggingTable();
+    };
 
-  return {
-    /**
-     *    console.table() your current localStorage
-     *    Set true or false to enable/disable
-     */
-    setDebuggingStatus,
-    /**
-     *    It try to access to corresponding property like
-     *    localStorage.getItem
-     */
-    tryGetValue,
+    let reset = () => {
+        localStorage.clear();
+        showDebuggingTable();
+    };
 
-    /**
-     *    It sets a new json compliant object
-     */
-    setValue,
+    return {
+        /**
+         *    console.table() your current localStorage
+         *    Set true or false to enable/disable
+         */
+        setDebuggingStatus,
+        /**
+         *    It try to access to corresponding property like
+         *    localStorage.getItem
+         */
+        tryGetValue,
 
-    /**
-     *    It removes the matching property value
-     */
-    removeValue,
+        /**
+         *    It sets a new json compliant object
+         */
+        setValue,
 
-    /**
-     *    It resets localStorage
-     */
-    reset
-  };
+        /**
+         *    It removes the matching property value
+         */
+        removeValue,
+
+        /**
+         *    It resets localStorage
+         */
+        reset
+    };
 };
 
 export default core;
